bf58cf45763a0753ca729e6d8c5eacf640edb1ee
Svetoslav Stoyanov <svetoslav39@gmai.com>
2024-05-09 01:08:48 +0300

    Add some plugins

---
+++ lua/kickstart/plugins/nvterm.lua
@@ -0,0 +1,72 @@
+return {
+  'NvChad/nvterm',
+  config = function()
+    require('nvterm').setup {
+      terminals = {
+        shell = vim.o.shell,
+        list = {},
+        type_opts = {
+          float = {
+            relative = 'editor',
+            row = 0.15,
+            col = 0.1,
+            width = 0.7,
+            height = 0.6,
+            border = 'single',
+          },
+          horizontal = { location = 'rightbelow', split_ratio = 0.3 },
+          vertical = { location = 'rightbelow', split_ratio = 0.5 },
+        },
+      },
+      behavior = {
+        autoclose_on_quit = {
+          enabled = false,
+          confirm = true,
+        },
+        close_on_exit = true,
+        auto_insert = true,
+      },
+    }
+
+    local terminal = require 'nvterm.terminal'
+    local ft_cmds = {
+      python = 'python3 ' .. vim.fn.expand '%',
+    }
+
+    local toggle_modes = { 'n', 't' }
+    local mappings = {
+      {
+        'n',
+        '<C-l>',
+        function()
+          terminal.send(ft_cmds[vim.bo.filetype])
+        end,
+      },
+      {
+        toggle_modes,
+        '<A-h>',
+        function()
+          terminal.toggle 'horizontal'
+        end,
+      },
+      {
+        toggle_modes,
+        '<A-v>',
+        function()
+          terminal.toggle 'vertical'
+        end,
+      },
+      {
+        toggle_modes,
+        '<A-i>',
+        function()
+          terminal.toggle 'float'
+        end,
+      },
+    }
+    local opts = { noremap = true, silent = true }
+    for _, mapping in ipairs(mappings) do
+      vim.keymap.set(mapping[1], mapping[2], mapping[3], opts)
+    end
+  end,
+}
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -48,6 +48,18 @@ require('lazy').setup({
 
   require 'kickstart/plugins/treesitter',
 
+  require 'kickstart.plugins.neo-tree',
+
+  require 'kickstart.plugins.smooth-scroll',
+
+  require 'kickstart.plugins.nvterm',
+
+  require 'kickstart.plugins.lazygit',
+
+  require 'kickstart.plugins.autopairs',
+
+  require 'kickstart.plugins.indent_line',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
